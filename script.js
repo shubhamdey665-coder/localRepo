bd9413127864ca713d09d5f59d0afca52615dd2b
shubhamdey665-coder <shubhamdey665@gmail.com>
2026-07-22 21:26:48 +0530

    22julyy 26

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-console.log("hello");
-age =10;
-console.log(age);
+let no =prompt("Enter a number:");
+if(no==0)
+{
+    console.log("The number is zero");
+}
+else if(no>0)
+{
+    console.log("the number is positive");
+}
+else
+{
+    console.log("the number is negative");
+}
